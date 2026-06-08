990427f43a6048816ef4fed4858e9c075cd3ccf8
Greg-alm <a.gregory@escola.pr.gov.br>
2026-06-08 11:49:05 -0300

    Update loja.py

--- loja.py
+++ loja.py
@@ -7,6 +7,24 @@ class cliente:
     self.__credito = credito
 
 
-    def carroVendido(self,credito)
+    
       
+class carroVendido:
+  def __init__(self,ano, modelo, quilometragem, valor_venda):
+    self.__ano = ano
+    self.__modelo = modelo
+    self.__quilometragem = quilometragem
+    self.__valor_venda = valor_venda
+
+
+
+
+
+
+class carroNovo:
+  def __init__(self,ano, modelo, quilometragem, valor_novo):
+    self.__ano = ano
+    self.__modelo = modelo
+    self.__quilometragem = quilometragem
+    self.__valor_novo = valor_novo
     
