adb3d62da0ed5f882225e4194e21113687999f98
Greg-alm <a.gregory@escola.pr.gov.br>
2026-06-08 11:48:21 -0300

    Add files via upload

---
+++ loja.py
@@ -0,0 +1,12 @@
+class cliente:
+   def __init__(self,nome,cpf,dataNascimento,saldo,credito):
+    self.__nome = nome
+    self.__cpf = cpf
+    self.__dataNascsimento = dataNascimento
+    self.__saldo = saldo
+    self.__credito = credito
+
+
+    def carroVendido(self,credito)
+      
+    
